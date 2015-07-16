203be434e9ec0b9a2d8b183770149a45d7c96f8b
K.-Michael Aye <michaelaye@users.noreply.github.com>
2015-07-15 20:37:01 -0600

    More comments about my role.

--- setup.py
+++ setup.py
@@ -9,7 +9,11 @@ setup(
 
     # metadata
     author="hitran.org",
+    # I am not the author of the physcis code in this package.
+    # I'm only the packager. And added a notebook or two.
     author_email="michael.aye@lasp.colorado.edu",
+    # author email just set to mine to avoid HITRAN team to be bothered by questions
+    # about this package. 
     description="Software for interfacing the HITRAN database",
     license="BSD 3-clause",
     keywords="HITRAN",
